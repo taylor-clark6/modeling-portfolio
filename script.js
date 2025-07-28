c744e557507a8689064a85abec51eebc3115de93
taylor-clark6 <116758714+taylor-clark6@users.noreply.github.com>
2025-07-28 17:50:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Add interactive enhancements later (e.g., gallery sliders, form handling)
 console.log("Portfolio loaded!");
-AOS.init();
+
 document.getElementById("backToTop").addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+AOS.init();
